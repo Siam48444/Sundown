cdde0974676132cdb75b2d9999b5bc87175fefbd
Siam <siam48444@gmail.com>
2024-06-26 18:04:55 +0600

    main

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ gsap.ticker.add((time) => {
 });
 
 gsap.ticker.lagSmoothing(0);
+
+// Hamburger button.
+document.querySelector(".hamburger_button").addEventListener("click", () => {
+    document.querySelectorAll(".hamburger_line")[0].classList.toggle("line_1_clicked");
+    document.querySelectorAll(".hamburger_line")[1].classList.toggle("line_2_clicked");
+});
