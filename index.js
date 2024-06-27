61f6fdbaf6e3098b89c5c3cdf8d1a514e5f32484
Siam <siam48444@gmail.com>
2024-06-27 21:13:54 +0600

    main

---
+++ index.js
@@ -0,0 +1,12 @@
+// Project hover animation.
+const project = document.querySelectorAll(".projects");
+const project_visual = document.querySelectorAll(".project_visual");
+
+for (let i = 0; i < project.length; i++) {
+    project[i].addEventListener("mouseenter", () => {
+        project_visual[i].classList.add("project_visual_open");
+    });
+    project[i].addEventListener("mouseleave", () => {
+        project_visual[i].classList.remove("project_visual_open");
+    });
+}
