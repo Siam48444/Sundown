4adef7755ed6a270a6d24ab1158db6e1cbb8fc3f
Siam <siam48444@gmail.com>
2024-06-28 12:10:08 +0600

    main

--- main.js
+++ main.js
@@ -24,7 +24,12 @@ document.querySelector(".hamburger_button").addEventListener("click", () => {
 // Load animation.
 var nav_height = document.querySelector("nav").clientHeight;
 tl.from("nav", { y: -(nav_height + 35), duration: 1, delay: 1, esae: "power1.out" }, "main");
-tl.from("main .char", { y: "101%", duration: 1, delay: 1, esae: "power1.out" }, "main");
+tl.from("main .word .char", { y: "101%", duration: 1, delay: 1, esae: "power1.out" }, "main");
 tl.from(".below_main_section", { opacity: 0, duration: 1, esae: "power1.out" });
 
 // Scroll appear animation.
+const scroll_appear_char = document.querySelectorAll(".scroll_appear .word .char");
+
+scroll_appear_char.forEach((scroll_char) => {
+    gsap.from(scroll_char, { y: "101%", duration: 1, delay: 1, esae: "power1.out", scrollTrigger: {trigger: scroll_char, strat: "top 70%"} });
+});
