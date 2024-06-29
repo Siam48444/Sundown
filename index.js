9de20520ba5dcfc519cb5138e0e2103eff42a73a
Siam <siam48444@gmail.com>
2024-06-29 17:55:26 +0600

    main

--- index.js
+++ index.js
@@ -22,17 +22,31 @@ design_image[0].classList.add("design_image_active");
 
 for (let a = 0; a < design_button.length; a++) {
     design_button[a].addEventListener("click", () => {
-        design_button.forEach((btn) => {btn.classList.remove("design_button_active");})
+        design_button.forEach((btn) => {
+            btn.classList.remove("design_button_active");
+        });
         design_button[a].classList.add("design_button_active");
 
-        design_buttons_line.forEach((line) => {line.classList.remove("design_button_line_active");})
+        design_buttons_line.forEach((line) => {
+            line.classList.remove("design_button_line_active");
+        });
         design_buttons_line[a].classList.add("design_button_line_active");
 
-        design_image.forEach((img) => {img.classList.remove("design_image_active");})
+        design_image.forEach((img) => {
+            img.classList.remove("design_image_active");
+        });
         design_image[a].classList.add("design_image_active");
-    })
+    });
 }
 
-// 
-// 
-// 
+//
+//
+//
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
